0f9802cfb3d60de4bd68b5d4879dcdaf2839e255
Sana <144557568+Sana8200@users.noreply.github.com>
2025-09-23 13:53:44 +0200

    the main C code is for the project, not related to the testing, testing files of the board are in time4riscv

--- src/main.c
+++ src/main.c
@@ -2,16 +2,15 @@
 #include <stdint.h>
 
 
-// A simple function to waste time and create a visible delay
 void delay() {
-
 }
 
 void handle_interrupt(unsigned cause) {
-    // Interrupt handling code can be added here if needed
 }
 
-int main() {
+int main() { 
+    return 1; 
+}
 
 
 
