445fbcd20a6436171ed21e4e0a2d0cab6a8370c6
Sana <144557568+Sana8200@users.noreply.github.com>
2025-10-01 16:35:56 +0200

    Added the function for turning on or off the LEDS

--- funcitons/leds.c
+++ funcitons/leds.c
@@ -1,10 +1,13 @@
 
 
+#define LED_BASE_ADDR 0x04000000
+
+
 void set_leds(int led_mask){
     
     // Creating a volatile pointer to the LED address 
     // volatile : tells the compiler don't optimize since the address might change at this pointer 
-    volatile int * led_pointer = (volatile int *) 0x04000000;
+    volatile int * led_pointer = (volatile int *) LED_BASE_ADDR;
 
     // Defrenece the pointer to write the led_mask value to the hardware address, turning the LEDs on or off 
     *led_pointer = led_mask;
@@ -12,4 +15,4 @@ void set_leds(int led_mask){
 }
 
 // Calling the function in the main.c 
-// set_leds(0x2A);    0x2A is an exmple, we can modify it to turn on the desired leds 
+// set_leds(0x2A);    0x2A is an exmple, we can modify it to turn on the desired leds  
