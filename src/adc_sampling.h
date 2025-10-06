982e729c0e706b68814115bb7362080a171e253a
Rachel-Lachiani <lr.lachiani@gmail.com>
2025-10-06 16:25:57 +0200

    Update adc_sampling.h
    added a function for sample adc and update buffer

--- src/adc_sampling.h
+++ src/adc_sampling.h
@@ -1,1 +1,15 @@
+#ifndef ADC_SAMPLING_H
+#define ADC_SAMPLING_H
+
+#include <stdint.h>
+
+/**
+ * @brief Reads a single 16-bit sample from the AD7705 using SPI and stores it
+ * in the global circular waveform buffer.
+ * * NOTE: This function is intended to be called by a high-frequency Timer Interrupt
+ * Service Routine (ISR).
+ */
+void sample_adc_and_update_buffer();
+
+#endif // ADC_SAMPLING_H
 
