994ad7c563ac0fa08599d948c1dc34a636e6ce5d
Rachel-Lachiani <lr.lachiani@gmail.com>
2025-10-06 15:01:54 +0200

    Update vga_driver.h
    Declare hardware functions with comments

--- src/vga_driver.h
+++ src/vga_driver.h
@@ -1,5 +1,12 @@
 #ifndef VGA_DRIVER_H
 #define VGA_DRIVER_H
 
-#include "framebuffer.h" // Now includes the header directly from the same 'src' folder
+#include "framebuffer.h" // Now includes the header directly from the same src folder
+// The hardware functions are now declared in main.c, but we 
+// still declare them here so vga_driver.c so we know they exist!
+extern void GPIO_set_pixel_data(Color color);
+extern void GPIO_set_hsync(int state);
+extern void GPIO_set_vsync(int state);
+extern void delay_us(float us);
+
 #endif // // VGA_DRIVER_H
